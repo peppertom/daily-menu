bf17a58dbd76d2e5d4a3e3261988cea7b81bf24a
Tamás Vigh <tamas@y-collective.hu>
2017-10-12 10:12:11 +0200

    add trafik pecs to restaurants

---
+++ restaurants/trafik.js
@@ -0,0 +1,16 @@
+const cheerio = require('cheerio');
+const he = require('he');
+
+module.exports = {
+	name: 'Trafik',
+	url: 'https://www.facebook.com/trafikapollo/',
+	getMenu: body => {
+		const $ = cheerio.load(body);
+		const dailyMenu = $('.userContent').filter((i,e) => {
+			return e.children.length;
+		}).eq(0).find('.text_exposed_root').text();
+
+		return dailyMenu.split(' - ')
+			.map(el => he.decode(el));
+	}
+};

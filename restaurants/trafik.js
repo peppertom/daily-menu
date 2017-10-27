68767442e5e3c3753f464d6afa9230ac2b59292e
peppertom <fxmatyi@gmail.com>
2017-10-28 00:12:43 +0200

    update trafik restaurant's scraping logic and extraction

--- restaurants/trafik.js
+++ restaurants/trafik.js
@@ -1,16 +1,30 @@
 const cheerio = require('cheerio');
 const he = require('he');
+const striptags = require('striptags');
 
 module.exports = {
 	name: 'Trafik',
-	url: 'https://www.facebook.com/trafikapollo/',
+	url: 'https://www.facebook.com/pg/trafikapollo/posts/?ref=page_internal',
 	getMenu: body => {
 		const $ = cheerio.load(body);
-		const dailyMenu = $('.userContent').filter((i, e) => {
-			return e.children.length;
-		}).eq(0).find('.text_exposed_root').text();
+		const separators = ['<br>', '<p>', '</span>', ' - '];
+		const dailyMenu = $('.userContent')
+			.filter((i, e) => e.children.length)
+			.filter((i, e) => $(e).text().indexOf(' mai ebéd') !== -1 || $(e).text().indexOf(' mai menü ') !== -1)
+			.eq(0)
+			.find('.text_exposed_root')
+			.html();
 
-		return dailyMenu.split(' - ')
-			.map(el => he.decode(el));
+		return dailyMenu
+			.split(new RegExp(separators.join('|'), 'g'))
+			.map(el => el.trim())
+			.map(el => striptags(el))
+			.map(el => he.decode(el))
+			.map(el => el.replace(/Trafik/g, ''))
+			.map(el => el.replace(/mai ebéd!/g, ''))
+			.map(el => el.replace(/mai menü!/g, ''))
+			.map(el => el.replace(/Jó étvágyat!/g, ''))
+			.map(el => el.replace(/Továbbiak/g, ''))
+			.map(el => el.replace(/\.\.\./g, ''));
 	}
 };
